5aa75e092ca3409a7cc8d8a0a79e7d1503edb13b
root <rayaitsimran@gmail.com>
2023-05-31 15:51:28 +0530

    initial commit

---
+++ wrapper.php
@@ -0,0 +1,39 @@
+<?php
+ 
+
+function wrp($choc,$wrpr){
+
+if($choc<$wrpr){
+    return 0;
+}
+$choc1=($choc/$wrpr);
+echo $choc1."<br>";
+
+ $choc= ($choc1+wrp($choc1,$wrpr));
+ return $choc;
+
+}
+function totalCh($money,$price,$wrpr){
+    if($money%$price==0){
+$choc=$money/$price;
+    }
+    else{
+        $choc=($money/$price)-0.5;
+        
+    }
+
+$total=ceil($choc+wrp($choc,$wrpr));
+
+
+echo ceil($total);
+
+
+}
+
+$money=10;
+$price=2;
+$w=2;
+
+ totalCh($money,$price,$w);
+
+?>
